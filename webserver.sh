2763acdd982a5fe0f0801b47a5ee376aeadbb4bf
MangoSpot <52512049+mangospot-net@users.noreply.github.com>
2019-07-16 00:30:15 +0700

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -3,5 +3,6 @@ echo ">>> Installing WebServer <<<"
 sudo apt-get install -y apache2 php php-curl php-mbstring php-xml php-gd php-dev php-pear php-ssh2 libmcrypt-dev mcrypt make
 sleep 2
 sudo pecl install mcrypt-1.0.1
+echo -ne '\n'
 echo ">>> Finished Installing WebServer"
 sleep 2
